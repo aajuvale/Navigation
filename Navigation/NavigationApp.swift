bc892870cf4214193082bf5d129ab5d22db511cb
aajuvale <ahmed.juvale@gmail.com>
2025-08-04 20:51:43 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ahmed Juvale on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
